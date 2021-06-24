b6d227e69c4cc61ea00d2e50328fddfe7cf90106
miggeess-test <miggeess@yahoo.com.mx>
2021-06-23 23:42:25 -0600

    Lenguaje tabla admin; descrpcion de los campos USR y ADMIN

--- scdd/src/main/resources/static/js/aprovarSolicitudes/aprovarSolicitud.js
+++ scdd/src/main/resources/static/js/aprovarSolicitudes/aprovarSolicitud.js
@@ -55,7 +55,193 @@ function servicioSolicitudTransporte() {
 function cargarTablaAdmin(datos) {
 	$('#tblSolicitudTransporte').DataTable(
         {
-            searching: false,
+        	rowCallback: function(row, data, index){
+        		console.log("Data : " + row);
+        		console.log("Data : " + data);
+        		console.log("Data : " + index);
+        		if(data.estatus == 'Creada'){
+        			//$(row).find('td:eq(3)').css('color', 'blue');
+        			$(row).css('color', 'blue');
+        		}
+        		if(data.estatus == 'Aprobada'){
+        			$(row).css('color', 'green');
+        			//$(row).find('td:eq(Aprobada)').css('color', 'green');
+        			//$(row).find('td:eq(2)').css('color', 'blue');
+        		}
+        	},
+        	"language": {
+        		
+    "processing": "Procesando...",
+    "lengthMenu": "Mostrar _MENU_ registros",
+    "zeroRecords": "No se encontraron resultados",
+    "emptyTable": "Ningún dato disponible en esta tabla",
+    "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+    "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+    "search": "Buscar:",
+    "infoThousands": ",",
+    "loadingRecords": "Cargando...",
+    "paginate": {
+        "first": "Primero",
+        "last": "Último",
+        "next": "Siguiente",
+        "previous": "Anterior"
+    },
+    "aria": {
+        "sortAscending": ": Activar para ordenar la columna de manera ascendente",
+        "sortDescending": ": Activar para ordenar la columna de manera descendente"
+    },
+    "buttons": {
+        "copy": "Copiar",
+        "colvis": "Visibilidad",
+        "collection": "Colección",
+        "colvisRestore": "Restaurar visibilidad",
+        "copyKeys": "Presione ctrl o u2318 + C para copiar los datos de la tabla al portapapeles del sistema. <br \/> <br \/> Para cancelar, haga clic en este mensaje o presione escape.",
+        "copySuccess": {
+            "1": "Copiada 1 fila al portapapeles",
+            "_": "Copiadas %d fila al portapapeles"
+        },
+        "copyTitle": "Copiar al portapapeles",
+        "csv": "CSV",
+        "excel": "Excel",
+        "pageLength": {
+            "-1": "Mostrar todas las filas",
+            "1": "Mostrar 1 fila",
+            "_": "Mostrar %d filas"
+        },
+        "pdf": "PDF",
+        "print": "Imprimir"
+    },
+    "autoFill": {
+        "cancel": "Cancelar",
+        "fill": "Rellene todas las celdas con <i>%d<\/i>",
+        "fillHorizontal": "Rellenar celdas horizontalmente",
+        "fillVertical": "Rellenar celdas verticalmentemente"
+    },
+    "decimal": ",",
+    "searchBuilder": {
+        "add": "Añadir condición",
+        "button": {
+            "0": "Constructor de búsqueda",
+            "_": "Constructor de búsqueda (%d)"
+        },
+        "clearAll": "Borrar todo",
+        "condition": "Condición",
+        "conditions": {
+            "date": {
+                "after": "Despues",
+                "before": "Antes",
+                "between": "Entre",
+                "empty": "Vacío",
+                "equals": "Igual a",
+                "notBetween": "No entre",
+                "notEmpty": "No Vacio",
+                "not": "Diferente de"
+            },
+            "number": {
+                "between": "Entre",
+                "empty": "Vacio",
+                "equals": "Igual a",
+                "gt": "Mayor a",
+                "gte": "Mayor o igual a",
+                "lt": "Menor que",
+                "lte": "Menor o igual que",
+                "notBetween": "No entre",
+                "notEmpty": "No vacío",
+                "not": "Diferente de"
+            },
+            "string": {
+                "contains": "Contiene",
+                "empty": "Vacío",
+                "endsWith": "Termina en",
+                "equals": "Igual a",
+                "notEmpty": "No Vacio",
+                "startsWith": "Empieza con",
+                "not": "Diferente de"
+            },
+            "array": {
+                "not": "Diferente de",
+                "equals": "Igual",
+                "empty": "Vacío",
+                "contains": "Contiene",
+                "notEmpty": "No Vacío",
+                "without": "Sin"
+            }
+        },
+        "data": "Data",
+        "deleteTitle": "Eliminar regla de filtrado",
+        "leftTitle": "Criterios anulados",
+        "logicAnd": "Y",
+        "logicOr": "O",
+        "rightTitle": "Criterios de sangría",
+        "title": {
+            "0": "Constructor de búsqueda",
+            "_": "Constructor de búsqueda (%d)"
+        },
+        "value": "Valor"
+    },
+    "searchPanes": {
+        "clearMessage": "Borrar todo",
+        "collapse": {
+            "0": "Paneles de búsqueda",
+            "_": "Paneles de búsqueda (%d)"
+        },
+        "count": "{total}",
+        "countFiltered": "{shown} ({total})",
+        "emptyPanes": "Sin paneles de búsqueda",
+        "loadMessage": "Cargando paneles de búsqueda",
+        "title": "Filtros Activos - %d"
+    },
+    "select": {
+        "1": "%d fila seleccionada",
+        "_": "%d filas seleccionadas"
+    },
+    "thousands": ".",
+    "datetime": {
+        "previous": "Anterior",
+        "next": "Proximo",
+        "hours": "Horas",
+        "minutes": "Minutos",
+        "seconds": "Segundos",
+        "unknown": "-",
+        "amPm": [
+            "am",
+            "pm"
+        ]
+    },
+    "editor": {
+        "close": "Cerrar",
+        "create": {
+            "button": "Nuevo",
+            "title": "Crear Nuevo Registro",
+            "submit": "Crear"
+        },
+        "edit": {
+            "button": "Editar",
+            "title": "Editar Registro",
+            "submit": "Actualizar"
+        },
+        "remove": {
+            "button": "Eliminar",
+            "title": "Eliminar Registro",
+            "submit": "Eliminar",
+            "confirm": {
+                "_": "¿Está seguro que desea eliminar %d filas?",
+                "1": "¿Está seguro que desea eliminar 1 fila?"
+            }
+        },
+        "error": {
+            "system": "Ha ocurrido un error en el sistema (<a target=\"\\\" rel=\"\\ nofollow\" href=\"\\\">Más información&lt;\\\/a&gt;).<\/a>"
+        },
+        "multi": {
+            "title": "Múltiples Valores",
+            "info": "Los elementos seleccionados contienen diferentes valores para este registro. Para editar y establecer todos los elementos de este registro con el mismo valor, hacer click o tap aquí, de lo contrario conservarán sus valores individuales.",
+            "restore": "Deshacer Cambios",
+            "noMulti": "Este registro puede ser editado individualmente, pero no como parte de un grupo."
+        }
+    },
+    "info": "Mostrando de _START_ a _END_ de _TOTAL_ entradas"
+        	},
+            searching: true,
             data: datos,
             select: true,
             "columns": [
@@ -139,7 +325,7 @@ function llenaVehiculos(data, listaHtml){
 			var option = document.createElement("option");
 			option.value = data[od].codigoVehiculo;
     		option.text = data[od].txtMarca + " | " + data[od].txtNombre;
-    		listaHtml.appendChild(option);
+    		listaHtml.append(option);
 		}
 	} else {
 		listaHtml.selectedIndex = "0";
@@ -152,7 +338,7 @@ function llenaOperadores(data, listaHtml){
 			var option = document.createElement("option");
 			option.value = data[od].codigoOperador;
     		option.text = data[od].txtNombre + " | " + data[od].txtApellidos;
-    		listaHtml.appendChild(option);
+    		listaHtml.append(option);
 		}
 	} else {
 		listaHtml.selectedIndex = "0";
@@ -160,8 +346,8 @@ function llenaOperadores(data, listaHtml){
 }
 
 function llenarSolicitudAdmin(data) {
-	llenaVehiculos(data.vehiculos, document.getElementById("listaVehiculos"));
-	llenaOperadores(data.operadores, document.getElementById("listaOperadores"));
+	llenaVehiculos(data.vehiculos, $("#listaVehiculos"));
+	llenaOperadores(data.operadores, $("#listaOperadores"));
 
 	$("#txtNombre").val(data.solicitud[0].nomSolicitante);
 	$("#txtFolio").val(data.solicitud[0].folioSolicitud);
@@ -236,13 +422,19 @@ function llenarSolicitudAdmin(data) {
 	if(data.solicitud[0].estatus != "Creada"){
 		console.log("solicitud aceptada, denegada, guardada");
 
-		document.getElementById("listaVehiculos").style.display="none";
-		document.getElementById("listaOperadores").style.display="none";
-		document.getElementById("txtVehiculoAsignado").value = data.solicitud[0].vehiculoAsignado;
-		document.getElementById("txtVehiculoAsignado").style.display="block";
+		//document.getElementById("listaVehiculos").style.display="none";
+		$("#listaVehiculos").hide();
+		//document.getElementById("listaOperadores").style.display="none";
+		$("#listaOperadores").hide();
+		//document.getElementById("txtVehiculoAsignado").value = data.solicitud[0].vehiculoAsignado;
+		$("#txtVehiculoAsignado").val(data.solicitud[0].vehiculoAsignado);
+		//document.getElementById("txtVehiculoAsignado").style.display="block";
+		$("#txtVehiculoAsignado").show();
 		$("#txtVehiculoAsignado").prop("disabled", true );
-		document.getElementById("txtOperadorAsignado").value = data.solicitud[0].nomOperador;
-		document.getElementById("txtOperadorAsignado").style.display="block";
+		//document.getElementById("txtOperadorAsignado").value = data.solicitud[0].nomOperador;
+		$("#txtOperadorAsignado").val(data.solicitud[0].nomOperador);
+		//document.getElementById("txtOperadorAsignado").style.display="block";
+		$("#txtOperadorAsignado").show();
 		$("#txtOperadorAsignado").prop("disabled", true );
 
 		$("#idConstrolVehicular").val(data.solicitud[0].idControlVehicular);
